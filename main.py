d940f31fc2c9a5d9913ceacf08b5d82da04fa575
Your Name <827511@mystma.org>
2021-10-05 15:26:19 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-#WRITE YOUR CODE IN THIS FILE
+def hasL(w):
+    
+    x = 0
+    
+    while x < len(w):
+        if w[x] == "l":
+            return True
+             
+        x = x + 1
+    return False
+
+        
+
+print(hasL("length"))
